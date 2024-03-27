1cc25635ab5eb814ca35833ad3c54e122a71e64e
kamal <kamal@onelap.in>
2024-03-27 16:21:18 +0530

    configure h2 database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ insert into users values('user','{noop}pa$$word','user@gmail.com',true);
 insert into users values('admin','{noop}god','admin@gmail.com',true);
 insert into authorities values('user','USER');
 insert into authorities values('admin','ADMIN');
-insert into transactions values(1,'user','BTC')
+insert into transactions values(1,'user','BTC');
